499b816844d1454996d0484bcbc9e1e982d230e6
Colin Su <colinwithpower@gmail.com>
2025-06-09 11:47:17 +0800

    added 5 more restaurants

--- README.md
+++
@@ -1,2 +0,0 @@
-# Codepath-WEB-102
-This is the repository for the course work (weekly after-class projects) of Codepath WEB102
--- my-community-board/src/App.jsx
+++ my-community-board/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 import RestaurantCard from './components/RestaurantCard';
 
-
 import ambar from './assets/ambar.png';
 import ichiban from './assets/ichiban.png';
 import shawarma from './assets/shawarma.png';
 import signature from './assets/signature.png';
 import bangkok from './assets/bangkok.png';
-
+import chopstix from './assets/chopstix.png';
+import manolos from './assets/manolos.png';
+import j6 from './assets/j6.png';
+import forage from './assets/forage.png';
+import dubai from './assets/dubai.png';
 
 const data = [
   {
@@ -44,6 +47,41 @@ const data = [
     address: "410 E Green St",
     image: bangkok,
     link: "http://yelp.com/apple_maps_action?id=5_cn7M-9swHrFbnqZtYGBQ&component=review&action=view&language=en-US,zh-Hant-US"
+  },
+  {
+    name: "Chopstix",
+    type: "Chinese",
+    address: "202 E Green St",
+    image: chopstix,
+    link: "http://www.chopstix217.com/"
+  },
+  {
+    name: "Manolo’s Pizza & Empanadas",
+    type: "Italian",
+    address: "1115 W Oregon St",
+    image: manolos,
+    link: "https://manolospizza.com/"
+  },
+  {
+    name: "J6 Kimbap",
+    type: "Korean",
+    address: "516 E John St",
+    image: j6,
+    link: "https://j6kimbap.square.site/"
+  },
+  {
+    name: "Forage Kitchen",
+    type: "Salad Bar",
+    address: "503 E Green St",
+    image: forage,
+    link: "https://www.eatforage.com/"
+  },
+  {
+    name: "Dubai Grill",
+    type: "Middle Eastern",
+    address: "701 S Gregory St",
+    image: dubai,
+    link: "https://www.dubai-grill.com/"
   }
 ];
 
